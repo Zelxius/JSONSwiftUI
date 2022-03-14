36453f412b27533fd6f01de9a3b5cc1625de762d
Kiara Itzel Laredo González <kiara_itzel@hotmail.com>
2022-03-14 12:33:00 -0600

    feat: get data form json

---
+++ JSON/Model/Modelo1.swift
@@ -0,0 +1,14 @@
+//
+//  Modelo1.swift
+//  JSON
+//
+//  Created by Colimasoft on 14/03/22.
+//
+
+import Foundation
+
+struct Modelo1: Decodable {
+    var id : Int
+    var name : String
+    var email : String
+}
--- JSON/View/Home.swift
+++ JSON/View/Home.swift
@@ -10,25 +10,34 @@ import SwiftUI
 struct Home: View {
     
     @EnvironmentObject var login: PostViewModel
+    @StateObject var json = Modelo1ViewModel()
     
     var body: some View {
+        
         NavigationView{
-            Text("Bienvenido")
-                .navigationBarTitle("JSON")
-                .navigationBarItems(leading:
-                    Button(action:{
-                        UserDefaults.standard.removeObject(forKey: "sesion")
-                        login.authenticated = 0
-                    }){
-                        Text("Salir")
+            if json.datosModelo.isEmpty {
+                ProgressView()
+            }else{
+                List(json.datosModelo, id:\.id){ item in
+                    VStack(alignment: .leading){
+                        Text(item.name)
+                        Text(item.email)
                     }
-                    ,trailing:
-                        Button(action:{
-                    }){
-                    Text("Siguiente")
-                }
-            )
+                }.navigationBarTitle("JSON")
+                .navigationBarItems(leading:
+                            Button(action:{
+                                UserDefaults.standard.removeObject(forKey: "sesion")
+                                login.authenticated = 0
+                            }){
+                                Text("Salir")
+                            }
+                            ,trailing:
+                                Button(action:{
+                            }){
+                            Text("Siguiente")
+                            }
+                )
+            }
         }
     }
-
 }
