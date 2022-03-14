a9710629a97aee875351f7dc18a2be1c71019911
Kiara Itzel Laredo González <kiara_itzel@hotmail.com>
2022-03-14 12:43:30 -0600

    feat: add sub levels

--- JSON/Model/Modelo1.swift
+++ JSON/Model/Modelo1.swift
@@ -11,4 +11,14 @@ struct Modelo1: Decodable {
     var id : Int
     var name : String
     var email : String
+    var address: Address
+}
+
+struct Address: Decodable {
+    var street : String
+    var geo : Geo
+}
+
+struct Geo: Decodable {
+    var lat: String
 }
